cf20673d8157d6cb5fdfbad6f1215d3cf91908b1
Can Ahmet ACAR <64232218+canacar7@users.noreply.github.com>
2022-01-09 13:32:50 +0300

    Update wallace_16X16.v

--- wallace_tree_16x16/wallace_16X16.v
+++ wallace_tree_16x16/wallace_16X16.v
@@ -1,6 +1,6 @@
 module wallace_16X16(a,b,z);                             // 16 * 16 wt product
     input  [15:00] a;                                    // 16 bits
-    input  [15:00] b;                                    // "6 bits
+    input  [15:00] b;                                    // 16 bits
     output [31:00] z;                                    // product
     wire   [31:07] x;                                    // sum high
     wire   [31:07] y;                                    // carry high
